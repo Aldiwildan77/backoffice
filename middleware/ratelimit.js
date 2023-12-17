0b6e11153db058d889fe7a3dd9d1a5e4eb221141
aldiwildan77 <aldiwild77@gmail.com>
2023-12-17 13:57:49 +0700

    chore: enhance rate limit

--- middleware/ratelimit.js
+++ middleware/ratelimit.js
@@ -1,10 +1,11 @@
 const { rateLimit } = require('express-rate-limit');
 
 const limiter = rateLimit({
-  limit: 50,
+  limit: 75,
   windowMs: 15 * 60 * 1000,
   standardHeaders: 'draft-7',
   legacyHeaders: false,
+  message: 'Too many requests from this IP, please try again after 15 minutes',
 });
 
 module.exports = { limiter };
